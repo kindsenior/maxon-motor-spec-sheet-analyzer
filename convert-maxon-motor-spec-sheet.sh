e9d4200dad27d3b6ca9237108539da4f7e05e6d5
kindsenior <kunio.einstein@gmail.com>
2015-05-22 22:21:17 -0700

    modify row number to extract

--- convert-maxon-motor-spec-sheet.sh
+++ convert-maxon-motor-spec-sheet.sh
@@ -1,1 +1,1 @@
-less hoge.pdf| grep -v ciency | sed -n '16,31p' | sed -e 's/cm2//' -e 's/[A-z,\/,%]//g' -e 's/(. \+)//' -e 's/\ \+//' | cut -f2- -d' ' | sed 's/ \+/,/g'
+less hoge.pdf| grep -v ciency | sed -n -e '16,23p;25,31p' | sed -e 's/cm2//' -e 's/[A-z,\/,%]//g' -e 's/(. \+)//' -e 's/\ \+//' | cut -f2- -d' ' | sed 's/ \+/,/g'
