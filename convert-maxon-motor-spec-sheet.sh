fc3cee2652d56a67c41d1447309f0fd0843b76fb
kindsenior <kunio.einstein@gmail.com>
2015-05-23 16:11:00 -0700

    add head row comma count

--- convert-maxon-motor-spec-sheet.sh
+++ convert-maxon-motor-spec-sheet.sh
@@ -1,1 +1,7 @@
-less hoge.pdf| grep -v ciency | sed -n -e '16,23p;25,31p' | sed -e 's/cm2//' -e 's/[A-z,\/,%]//g' -e 's/(. \+)//' -e 's/\ \+//' | cut -f2- -d' ' | sed 's/ \+/,/g'
+less hoge.pdf| grep -v ciency | sed -n -e '16,23p;25,31p' | sed -e 's/cm2//' -e 's/[A-z,\/,%]//g' -e 's/(. \+)//' -e 's/\ \+//' | cut -f2- -d' ' | sed 's/ \+/,/g' | cat > hoge.txt
+# head hoge.txt -n1 | sed 's/[^,]//g' | sed 's/.$//' | wc -m
+
+for i in `seq \`head hoge.txt -n1 | sed 's/[^,]//g' | sed 's/.$//' | wc -m\` `
+do
+		echo "hoge"
+done
