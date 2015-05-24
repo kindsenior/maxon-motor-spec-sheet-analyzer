3788c32eb34806e77fb116fcd84d493642e005f1
kindsenior <kunio.einstein@gmail.com>
2015-05-23 18:18:08 -0700

    add motor type row

--- convert-maxon-motor-spec-sheet.sh
+++ convert-maxon-motor-spec-sheet.sh
@@ -1,7 +1,9 @@
-less hoge.pdf| grep -v ciency | sed -n -e '16,23p;25,31p' | sed -e 's/cm2//' -e 's/[A-z,\/,%]//g' -e 's/(. \+)//' -e 's/\ \+//' | cut -f2- -d' ' | sed 's/ \+/,/g' | cat > hoge.txt
-# head hoge.txt -n1 | sed 's/[^,]//g' | sed 's/.$//' | wc -m
+echo "" | cat > hoge.txt
+less hoge.pdf| grep -v ciency | sed -n -e '16,23p;25,31p' | sed -e 's/cm2//' -e 's/[A-z,\/,%]//g' -e 's/(. \+)//' -e 's/\ \+//' | cut -f2- -d' ' | sed 's/ \+/,/g' | cat >> hoge.txt
 
-for i in `seq \`head hoge.txt -n1 | sed 's/[^,]//g' | sed 's/.$//' | wc -m\` `
+for i in `seq \`sed -n '2,2p' hoge.txt | sed -e 's/[^,]//g;s/$//' | wc -m\` `
 do
-		echo "hoge"
+		less hoge.pdf| sed -n 1,1p | sed -e 's/\,//g;s/ \+/ /g;s/^/,/' | paste - hoge.txt > tmp.txt
+		cat tmp.txt > hoge.txt
+		rm -f tmp.txt
 done
