45ab4f0f791f263c0aeb92646336a78da615262e
kindsenior <kunio.einstein@gmail.com>
2015-05-25 15:29:07 -0700

    implement data row head hash table

--- convert-maxon-motor-spec-sheet.sh
+++ convert-maxon-motor-spec-sheet.sh
@@ -14,7 +14,10 @@ function pushMotorData(){
 
 		# extract Motor Data Table
 		echo "" | cat > ${page_txt}
-		less ${page_pdf}| grep -v ciency | sed -n -e "${row0},${row1}p;${row2},${row3}p" | sed -e 's/cm2//;s/[A-z,\/,%]//g;s/(. \+)//;s/.)//;s/^ \+//;s/^[0-9]\+//;s/ \+/,/g' | cat >> ${page_txt}
+		for idx in ${row_heads[@]}
+		do
+				less ${page_pdf}| grep -v ciency | sed -n -e "${idx}p" | sed -e 's/cm2//;s/[A-z,\/,%]//g;s/(. \+)//;s/.)//;s/^ \+//;s/^[0-9]\+//;s/ \+/,/g' | cat >> ${page_txt}
+		done
 
 		# insert Motor Name to each column head
 		# for i in `seq \`sed -n '2,2p' ${page_txt} | sed -e 's/[^,]//g;s/$//' | wc -m\` `
@@ -31,22 +34,44 @@ function pushMotorData(){
 
 # check Motor Data Table
 # set row0-3
+declare -A row_heads
+row_heads["Rotor_inertia"]=""
+row_heads["Mechanical_time_constant"]=""
+row_heads["Speed_/_torque_gradient"]=""
+row_heads["Speed_constant"]=""
+row_heads["Torque_constant"]=""
+row_heads["Terminal_inductance_phase_to_phase"]=""
+row_heads["Terminal_resistance_phase_to_phase"]=""
+row_heads["Starting_current"]=""
+row_heads["Stall_torque"]=""
+row_heads["Nominal_current"]=""
+row_heads["Nominal_torque"]=""
+row_heads["Nominal_speed"]=""
+row_heads["No_load_current"]=""
+row_heads["No_load_speed"]=""
+row_heads["Nominal_voltage"]=""
+
 function detectMotorDataTable(){
+		echo "detectMotorDataTable" $1
 		page=$1
 
-		for i in `seq \`less ${page}.pdf|wc -l\``
+		for key in ${!row_heads[@]}
 		do
-				if [ `less ${page}.pdf|sed -n ${i}p|grep 'Motor Data'|wc -l` == 1 ]
-				then
-						export row0=`expr ${i} + 2`
-						break
-				fi
+				for i in $(seq $(less ${page}.pdf|wc -l))
+				do
+						if [ $(less ${page}.pdf|sed -n ${i}p|sed 's/\ /_/g'|grep ${key}|wc -l) == 1 ]
+						then
+								row_heads[${key}]=${i}
+								echo " " ${key} ":"${row_heads[${key}]}
+								break
+						fi
+				done
 		done
-		export row1=`expr ${row0} + 7`
-		export row2=`expr ${row0} + 9`
-		export row3=`expr ${row0} + 15`
+		# export row1=`expr ${row0} + 7`
+		# export row2=`expr ${row0} + 9`
+		# export row3=`expr ${row0} + 15`
 		
-		echo "row0: " ${row0} "  row1: " ${row1} "  row2: " ${row2} "  row3: " ${row3}
+		# echo "row0: " ${row0} "  row1: " ${row1} "  row2: " ${row2} "  row3: " ${row3}
 }
 
 # create row head
@@ -57,7 +82,7 @@ pdftk ${target}.pdf cat ${template_page} output ${template}.pdf
 echo "Done"
 detectMotorDataTable ${template}
 echo "Motor Type" | cat > ${target}.csv
-less ${template}.pdf| grep -v ciency | sed -n -e "${row0},${row1}p;${row2},${row3}p" | sed -e 's/^[0-9 ]\+//' -e 's/ \+/ /g' -e 's/[0-9,., ]\+$//g' >> ${target}.csv
+less ${template}.pdf| grep -v ciency | sed -n $(echo ${row_heads[@]}"p" | sed 's/ /p;/g') | sed -e 's/^[0-9 ]\+//' -e 's/ \+/ /g' -e 's/[0-9,., ]\+$//g' >> ${target}.csv
 rm -f ${template}.pdf
 
 # for page in seq 138 160
